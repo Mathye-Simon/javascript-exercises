85994127105ab75394673b2c44ade96376b103b2
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-06 19:45:04 +0200

    repeatString testing passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+function repeatString (string,num) {
+    
+    repeatedString = [];
+    // CHECK FIRST FOR NEGATIVES
+    if (num < 0) {
+        return 'ERROR'
+    }
+// pushing to an array so they can pile on each other multiple of times
+    for (i = 1; i <= num; i++) {
+        repeatedString.push(string)
+        
+    }
+    return(repeatedString.join(''))
+    
 };
 
+repeatString('hey',6)
 // Do not edit below this line
 module.exports = repeatString;
