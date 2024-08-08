5ba066f10b0fa3845d721961aeb37818d591154a
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-08 15:54:17 +0200

    used args instead of multiple arguements to improve efficiency

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,12 @@
-function removeFromArray (array, num, num2, num3, num4) {
-    if (num == '' || num2 == '') {
-        return array
-    }
-    for (i = 0; i < array.length; i++) {
-        // checks for what to remove then removes 1 from the index because the length has decreased
-        if(array[i] === num || array[i] === num2 || array[i] === num3 || array[i] === num4) {
-            array.splice(i,1)
-            i-=1
-        } 
-    } 
-    return array
-    
+function removeFromArray (array,...args) {
+    let newArray = []
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item)
+        }
+    })
+    return newArray
 };
 
 removeFromArray([1,2,3,4,5], 3, 2)
