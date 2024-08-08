13253ece78e487c7e07eed1ab12029c3df36754a
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-08 10:19:48 +0200

    all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,24 @@
-const reverseString = function() {
 
+function reverseString (string) {
+    // turn string to array allowing every charater to be accessed through loop
+    let splitstring = string.split('')
+    let reversedString = []
+    
+    if (string == '') {
+        return reversedString = ''
+    }
+    else {
+        for (i = splitstring.length -1; i >= 0;) {
+        
+        reversedString.push(splitstring[i])       
+        i -= 1         
+        }
+        reversedString = reversedString.join('')
+        return reversedString
+    }
+    
 };
+reverseString('hello there')
 
 // Do not edit below this line
 module.exports = reverseString;
