8c5e8d73d811cd99ceff92eeb189c32dce63ae29
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-08 18:35:54 +0200

    tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,23 @@
 const sumAll = function(num1,num2) {
     range = 0
-    let i = 0
     listOfNums= []
-    if (num1 >= num2){
-        while (i<=num1) {
-            i+=1
-            listOfNums.push(i)
+
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1)||!Number.isInteger(num2)){
+        return 'ERROR'
+    }
+
+    else if (num1 >= num2){
+        while (num2<=num1) {
+            
+            listOfNums.push(num2)
+            num2+=1
         }
     }
     else if (num2 >= num1) {
-        while (i < num2) {
-            i+=1
-            listOfNums.push(i)
+        while (num1 <= num2) {
+            
+            listOfNums.push(num1)
+            num1+=1
         }
     }
     listOfNums.forEach((item)=>{
