c5e0e3a4196219df6eceac986bb17370fe676fdc
Mathye-Simon <mathye.tsakane.simon@gmail.com>
2024-08-08 15:27:04 +0200

    all test suites passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+function removeFromArray (array, num, num2, num3, num4) {
+    if (num == '' || num2 == '') {
+        return array
+    }
+    for (i = 0; i < array.length; i++) {
+        // checks for what to remove then removes 1 from the index because the length has decreased
+        if(array[i] === num || array[i] === num2 || array[i] === num3 || array[i] === num4) {
+            array.splice(i,1)
+            i-=1
+        } 
+    } 
+    return array
+    
 };
 
+removeFromArray([1,2,3,4,5], 3, 2)
 // Do not edit below this line
 module.exports = removeFromArray;
